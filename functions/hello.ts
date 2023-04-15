20f2ec7ec1f98b263bba91a9800645d089d7eff2
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2023-04-15 15:35:32 -0300

    function test

--- functions/hello.ts
+++ functions/hello.ts
@@ -1,10 +1,31 @@
-import type { Handler } from "@netlify/functions";
+import { Handler } from "@netlify/functions";
+import { FilebaseClient } from "@filebase/client";
+import { StatusCodes } from "http-status-codes";
 
-export const handler: Handler = async (event) => {
-  const subject = event.queryStringParameters?.name || "World";
+const filebase = new FilebaseClient({
+  token:
+    "M0QzRDQyNEY4NEQ4OTEwN0U0MDU6UXRNOGJ2RkFxU0ZWWDNrRmZRcUE0Z0NDc1pKYzV1MEJBYWljVzRxbTp0ZXN0LWJ1Y2t5",
+});
+
+const pinToFilebase = async (msg: string) => {
+  const content = new Blob([msg]);
+  const cid = await filebase.storeBlob(content);
+  return cid;
+};
+
+const handler: Handler = async (event) => {
+  if (!event.body)
+    return {
+      statusCode: StatusCodes.BAD_REQUEST,
+      body: JSON.stringify({ message: "Invalid body format" }),
+    };
+
+  const cid = await pinToFilebase(event.body);
 
   return {
-    statusCode: 200,
-    body: `Hello ${subject}!`,
+    statusCode: StatusCodes.OK,
+    body: JSON.stringify({ cid }),
   };
 };
+
+export { handler };
